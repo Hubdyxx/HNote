f08d1cfdcc6610d9ca6951a67f47e9972616e41f
hubdyxx <hubdyxx@gmail.com>
2024-02-27 20:59:28 +0100

    0.2

---
+++ HNote/Views/HNButton.swift
@@ -0,0 +1,18 @@
+//
+//  HNButton.swift
+//  HNote
+//
+//  Created by Hubert Kotlarz on 27/02/2024.
+//
+
+import SwiftUI
+
+struct HNButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HNButton()
+}
